125ce544edb7ca1bf269ef202bb0ecda42e5acf7
jahn18 <john.ahn.es@gmail.com>
2021-03-06 15:47:09 -0800

    Implemented Websocket framework for the application

--- sailbot/apps.py
+++ sailbot/apps.py
@@ -3,10 +3,7 @@ from django.apps import AppConfig
 class SailbotConfig(AppConfig):
     name = 'sailbot'
     verbose_name = "My Application"
-    def ready(self):
-       # from threading import Thread
-       # import sailbot.network_table.index
-       # thread = Thread(target=sailbot.network_table.index.subscribe)
-       # thread.start()
-        from models import Wind
-        Wind.objects.filter(pk=1).update(WindSpeed=3731)
+   # def ready(self):
+   #     from threading import Thread
+   #     thread = Thread(target=sailbot.network_table.index)
+   #     thread.start()
