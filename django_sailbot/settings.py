e1f0ff0649dff0ceb056c0dca5ad13c36960af5d
jahn18 <john.ahn.es@gmail.com>
2021-04-14 01:08:54 -0700

    Completed javascript implementation

--- django_sailbot/settings.py
+++ django_sailbot/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_pdb',
     'sailbot.apps.SailbotConfig',
+    'livereload',
 ]
 
 MIDDLEWARE = [
@@ -50,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'livereload.middleware.LiveReloadScript'
 ]
 
 ROOT_URLCONF = 'django_sailbot.urls'
--- sailbot/static/sailbot/javascript/get_network_table_data.js
+++ sailbot/static/sailbot/javascript/get_network_table_data.js
@@ -1,3 +1,14 @@
+
+// All constants for the html values on the website
+var winch_motor_html = document.getElementById("Winch Motor").getElementsByTagName("TD");
+var wind_html = document.getElementById("Wind").getElementsByTagName("TD");
+var gps_html = document.getElementById("GPS").getElementsByTagName("TD");
+var boom_angle_html = document.getElementById("Boom Angle").getElementsByTagName("TD");
+var bms_html = document.getElementById("BMS").getElementsByTagName("TD");
+var rudder_motor_html = document.getElementById("Rudder Motor").getElementsByTagName("TD");
+var accelerometer_html = document.getElementById("Rudder Motor").getElementsByTagName("TD");
+
+
 var socket = new WebSocket('ws://localhost:8000/ws/networkTableData/');
 socket.onopen = function(e) {
     console.log('Connection established');
@@ -5,8 +16,6 @@ socket.onopen = function(e) {
 
 socket.onmessage = function(e) {
     let sensor_data = JSON.parse(e.data);
-    let wind_html = document.getElementById("Wind").getElementsByTagName("TD");
-    let gps_html = document.getElementById("GPS").getElementsByTagName("TD");
     switch(sensor_data.sensor_type) {
         case '/winch_motor_control_0':
             winch_motor_html[1].innerHTML = sensor_data.current;
@@ -51,8 +60,99 @@ socket.onmessage = function(e) {
             wind_html[28].innerHTML = sensor_data.status;
             break;
         case '/gps_0':
+            gps_html[1].innerHTML = sensor_data.quality_indicator;
+            gps_html[2].innerHTML = sensor_data.hdop;
+            gps_html[3].innerHTML = sensor_data.antenna_altitude;
+            gps_html[4].innerHTML = sensor_data.geoidal_separation;
+            gps_html[5].innerHTML = sensor_data.utc_timestamp;
             gps_html[6].innerHTML = sensor_data.latitude;
             gps_html[7].innerHTML = sensor_data.longitude;
+            gps_html[8].innerHTML = sensor_data.ground_speed;
+            gps_html[9].innerHTML = sensor_data.track_made_good;
+            gps_html[10].innerHTML = sensor_data.magnetic_variation;
+            gps_html[11].innerHTML = sensor_data.voltage;
+            break;
+        case '/boom_angle_sensor':
+            boom_angle_html[1].innerHTML = sensor_data.angle;
+            boom_angle_html[2].innerHTML = sensor_data.current;
+            boom_angle_html[3].innerHTML = sensor_data.voltage;
+            boom_angle_html[4].innerHTML = sensor_data.temperature;
+            boom_angle_html[5].innerHTML = sensor_data.status;
+            break;
+        case '/bms_0':
+            bms_html[1].innerHTML = sensor_data.battery_current;
+            bms_html[2].innerHTML = sensor_data.battery_temperature;
+            bms_html[3].innerHTML = sensor_data.battery_voltage;
+            bms_html[4].innerHTML = sensor_data.current;
+            bms_html[5].innerHTML = sensor_data.voltage;
+            bms_html[6].innerHTML = sensor_data.temperature;
+            bms_html[7].innerHTML = sensor_data.status;
+            break;
+        case '/bms_1':
+            bms_html[10].innerHTML = sensor_data.battery_current;
+            bms_html[11].innerHTML = sensor_data.battery_temperature;
+            bms_html[12].innerHTML = sensor_data.battery_voltage;
+            bms_html[13].innerHTML = sensor_data.current;
+            bms_html[14].innerHTML = sensor_data.voltage;
+            bms_html[15].innerHTML = sensor_data.temperature;
+            bms_html[16].innerHTML = sensor_data.status;
+            break;
+        case '/bms_2':
+            bms_html[19].innerHTML = sensor_data.battery_current;
+            bms_html[20].innerHTML = sensor_data.battery_temperature;
+            bms_html[21].innerHTML = sensor_data.battery_voltage;
+            bms_html[22].innerHTML = sensor_data.current;
+            bms_html[23].innerHTML = sensor_data.voltage;
+            bms_html[24].innerHTML = sensor_data.temperature;
+            bms_html[25].innerHTML = sensor_data.status;
+            break;
+        case '/bms_3':
+            bms_html[28].innerHTML = sensor_data.battery_current;
+            bms_html[29].innerHTML = sensor_data.battery_temperature;
+            bms_html[30].innerHTML = sensor_data.battery_voltage;
+            bms_html[31].innerHTML = sensor_data.current;
+            bms_html[32].innerHTML = sensor_data.voltage;
+            bms_html[33].innerHTML = sensor_data.temperature;
+            bms_html[34].innerHTML = sensor_data.status;
+            break;
+        case '/bms_4':
+            bms_html[37].innerHTML = sensor_data.battery_current;
+            bms_html[38].innerHTML = sensor_data.battery_temperature;
+            bms_html[39].innerHTML = sensor_data.battery_voltage;
+            bms_html[40].innerHTML = sensor_data.current;
+            bms_html[41].innerHTML = sensor_data.voltage;
+            bms_html[42].innerHTML = sensor_data.temperature;
+            bms_html[43].innerHTML = sensor_data.status;
+            break;
+        case '/bms_5':
+            bms_html[46].innerHTML = sensor_data.battery_current;
+            bms_html[47].innerHTML = sensor_data.battery_temperature;
+            bms_html[48].innerHTML = sensor_data.battery_voltage;
+            bms_html[49].innerHTML = sensor_data.current;
+            bms_html[50].innerHTML = sensor_data.voltage;
+            bms_html[51].innerHTML = sensor_data.temperature;
+            bms_html[52].innerHTML = sensor_data.status;
+            break;
+        case '/rudder_motor_control_0':
+            rudder_motor_html[1].innerHTML = sensor_data.current;
+            rudder_motor_html[2].innerHTML = sensor_data.voltage;
+            rudder_motor_html[3].innerHTML = sensor_data.temperature;
+            rudder_motor_html[4].innerHTML = sensor_data.status;
+            break;
+        case '/rudder_motor_control_1':
+            rudder_motor_html[7].innerHTML = sensor_data.current;
+            rudder_motor_html[8].innerHTML = sensor_data.voltage;
+            rudder_motor_html[9].innerHTML = sensor_data.temperature;
+            rudder_motor_html[10].innerHTML = sensor_data.status;
+            break;
+        case '/accelerometer':
+            accelerometer_html[1].innerHTML = sensor_data.x_pos;
+            accelerometer_html[2].innerHTML = sensor_data.y_pos;
+            accelerometer_html[3].innerHTML = sensor_data.z_pos;
+            accelerometer_html[4].innerHTML = sensor_data.current;
+            accelerometer_html[5].innerHTML = sensor_data.voltage;
+            accelerometer_html[6].innerHTML = sensor_data.temperature;
+            accelerometer_html[7].innerHTML = sensor_data.status;
         default:
             break;
     }
