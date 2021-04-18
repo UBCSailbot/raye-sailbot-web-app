8fe175cc95b421e1cc63d07a18be31f2bff3dd19
jahn18 <john.ahn.es@gmail.com>
2021-04-17 17:28:54 -0700

    Implemented waypoint interface on website

--- sailbot/static/sailbot/javascript/map.js
+++ sailbot/static/sailbot/javascript/map.js
@@ -1,8 +1,18 @@
-$(document).ready(function() {
-  mapboxgl.accessToken = '';
-  var mapObject = {
-      container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v11'
-  };
-  var map = new mapboxgl.Map(mapObject);
-});
+let map;
+let mark;
+function initMap() {
+    let lat = 49.26;
+    let lng = -123.24;
+    map = new google.maps.Map(document.getElementById("map"), {
+      center: { lat: lat , lng: lng },
+      zoom: 11,
+    });
+    mark = new google.maps.Marker({position:{lat:lat, lng:lng}, map:map});
+}
+/*
+var redraw = function(payload) {
+  lat = payload.message.lat;
+  lng = payload.message.lng;
+  map.setCenter({lat:lat, lng:lng, alt:0});
+  mark.setPosition({lat:lat, lng:lng, alt:0});
+};*/
